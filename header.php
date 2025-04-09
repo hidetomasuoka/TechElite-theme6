65f0c305426252014f49ea045e294c0052ebbb8e
Hideto Masuoka <hidemasuoka112@gmail.com>
2025-04-09 16:33:21 +0900

    作業用コミット

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/accordion.css">
     <link rel="stylesheet" href="css/logo-sizes.css">
+    <link rel="stylesheet" href="css/faq-style.css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <title>フリーランスサミット</title>
 </head>
